1b91ca851d631be207ea359d089de0a4b152f0ba
Thilan <thilanoftran@gmail.com>
2020-02-23 21:41:41 -0800

    added missing scheduling.js

---
+++ src/components/containers/Scheduling.js
@@ -0,0 +1,84 @@
+import React, { useState, useEffect } from 'react';
+
+import Task from '../presentational/Task';
+import { FullButton } from '../presentational/styled/Button';
+import { TaskSection } from '../presentational/Dropdown';
+
+const tempScheduling = [
+  {
+    id: 0,
+    name: 'Task 1',
+    duration: 60,
+    description: 'cool tasks',
+    created_date: '',
+    scheduled_date: new Date().toISOString(),
+    completed: false,
+    scheduled: false,
+    due_date: new Date().toISOString()
+  },
+  {
+    id: 1,
+    name: 'Task 2',
+    duration: 90,
+    description: 'a task',
+    created_date: '',
+    scheduled_date: new Date().toISOString(),
+    completed: false,
+    scheduled: false,
+    due_date: new Date().toISOString()
+  }
+];
+
+const SchedulingList = () => {
+  const [scheduling, setScheduling] = useState(tempScheduling);
+  const [errors, setErrors] = useState(tempScheduling);
+  const [selected, setSelected] = useState([]);
+
+  const deleteTask = (id) =>
+    setScheduling(scheduling.filter((task) => task.id !== id));
+
+  const selectTask = (id) => {
+    if (selected.indexOf(id) === -1) {
+      setSelected(selected.concat([id]));
+    } else {
+      setSelected(selected.filter((num) => num !== id));
+    }
+  };
+
+  return (
+    <div>
+      <h3>
+        We were able to find times in your schedule for the following{' '}
+        {scheduling.length} tasks:
+      </h3>
+      {scheduling.map((task) => (
+        <Task
+          key={task.id}
+          task={task}
+          deleteTask={deleteTask}
+          toggleSelect={selectTask}
+          selected={selected.indexOf(task.id) !== -1}
+        />
+      ))}
+      <h3>
+        With the current scheduling settings, we were unable to find time for
+        the following tasks:
+      </h3>
+      {scheduling.map((task) => (
+        <Task
+          key={task.id}
+          task={task}
+          deleteTask={deleteTask}
+          toggleSelect={selectTask}
+          selected={selected.indexOf(task.id) !== -1}
+        />
+      ))}
+      <FullButton onClick={() => {}}>
+        Confirm {selected.length ? selected.length : 'All'}{' '}
+        {selected.length === 1 ? 'Task' : 'Tasks'}
+      </FullButton>
+    </div>
+  );
+};
+
+export default SchedulingList;
